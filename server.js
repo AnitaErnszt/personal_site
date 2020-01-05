10f10202d069432bb285ec2ff6581c47fdf7ba87
AnitaErnszt <56001200+AnitaErnszt@users.noreply.github.com>
2020-01-05 22:03:16 +0000

    added CSS and JS to express framework

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const request = require("request")
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html")
@@ -14,17 +15,17 @@ app.post("/", function(req, res){
   var URL =
   request(
     { method: "POST"
-    , uri: "https://7jnrcr4ft5.execute-api.eu-west-1.amazonaws.com/dev/v1/auth/login"
+    , uri: "https://548ag3a1w3.execute-api.eu-west-1.amazonaws.com/test/logintest"
     , body: body
         }
     , function(error, response, body){
-        console.log(response.body);
+        console.log(response);
       });
 });
 
-// app.get("/jsgame", function(req, res){
-//   res.sendFile(__dirname + "/jsgame.html")
-// });
+app.get("/jsgame", function(req, res){
+  res.sendFile(__dirname + "/jsgame.html")
+});
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
