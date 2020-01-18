785d676abc33458c876b5ff0f8a2fdecbaddc9cd
AnitaErnszt <56001200+AnitaErnszt@users.noreply.github.com>
2020-01-18 01:14:11 +0000

    change PORT

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.post("/register", function(req, res){
       });
 });
 
-app.listen(3000, function(){
-  console.log("server started on port 3000");
+app.listen(80, function(){
+  console.log("server started on port 80");
 });
